8c8f4eb91f260bcb58ae3a7decb61e228d035f3c
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-04-14 20:59:37 +0300

    ft: add quotes array

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes: Quote[] = [
+    new Quote(" The only language that looks the same before and after RSA encryption.", "Keith Bostic", "James Njoroge"),
+    new Quote(" Two things are infinite: the universe and human stupidity.", "Albert Einsten", "James Njoroge"),
+    new Quote(" Programming is like sex: one mistake and you\u2019re providing support for a lifetime.", "Dick Brandon", "James Njoroge"),
+    new Quote(" I love deadlines. I like the whooshing sound they make as they fly by.", "Douglas Adams", "James Njoroge"),
+    new Quote(" You should name a variable using the same care with which you name a first-born child.", "James O. Coplien", "James Njoroge"),
+   
+  ]
 
   constructor() { }
 
